fa7414e1ea71ac5e63630a6d7a574eda11a984d0
Vilém Zouhar <vilem.zouhar@gmail.com>
2020-12-17 19:35:08 +0100

    exp1 graphs sentdot and docdot

--- src/processing/exp1_top.py
+++ src/processing/exp1_top.py
@@ -9,24 +9,29 @@ from sklearn.linear_model import LinearRegression
 parser = argparse.ArgumentParser()
 parser.add_argument('-s', '--per-sent', action='store_true', default=False)
 parser.add_argument('-m', '--mt-only', action='store_true', default=False)
+parser.add_argument('-a', '--aggregate-documents', action='store_true', default=False)
 args = parser.parse_known_args()[0]
 
 data = load_mx()
 SKIP_SRC_REF = args.mt_only
+AGGREGATE_DOCUMENTS = args.aggregate_documents
 PER_SENT = args.per_sent
 MAX_WORD_TIME = 40 
 MAX_SENT_TIME = MAX_WORD_TIME*20
 
 mt_times = {k:[] for k in sorted(MT_BLEU.keys(), key=lambda x: MT_BLEU[x][0])}
 for doc in data:
-    if PER_SENT:
-        doc_time_avg = np.average([x.edit_time for x in doc.lines])
-        if doc_time_avg <= MAX_SENT_TIME:
-            mt_times[doc.mt_name].append(doc_time_avg)
+    if not AGGREGATE_DOCUMENTS:
+        mt_times[doc.mt_name] += [x.edit_time_word for x in doc.lines if x.edit_time_word <= MAX_WORD_TIME]
     else:
-        doc_time_avg = np.average([x.edit_time_word for x in doc.lines])
-        if doc_time_avg <= MAX_WORD_TIME:
-            mt_times[doc.mt_name].append(doc_time_avg)
+        if PER_SENT:
+            doc_time_avg = np.average([x.edit_time for x in doc.lines])
+            if doc_time_avg <= MAX_SENT_TIME:
+                mt_times[doc.mt_name].append(doc_time_avg)
+        else:
+            doc_time_avg = np.average([x.edit_time_word for x in doc.lines])
+            if doc_time_avg <= MAX_WORD_TIME:
+                mt_times[doc.mt_name].append(doc_time_avg)
 
 def top_n(n, points=False):
     bleu_time = []
@@ -45,7 +50,7 @@ def top_n(n, points=False):
     poly1d_fn = np.poly1d(coef) 
 
     if points:
-        plt.plot(xval, yval, '.')
+        plt.plot(xval, yval, '.', alpha=0.5)
     plt.plot(xval, poly1d_fn(xval), label=f'Top {n:02}, {coef[0]:>6.3f}')
     explanation = (
         ('time per line = ' if PER_SENT else 'time per word = ') +
@@ -64,5 +69,6 @@ plt.title(
         ('(without src, ref)' if SKIP_SRC_REF else '(with src, ref)')
 )
 plt.xlabel('BLEU')
-plt.ylabel('average (per document) line edit time' if PER_SENT else 'average (per document) word edit time')
+perwhat = 'document' if AGGREGATE_DOCUMENTS else 'sentence'
+plt.ylabel(f'average (per {perwhat}) line edit time' if PER_SENT else f'average (per {perwhat}) word edit time')
 plt.show()
