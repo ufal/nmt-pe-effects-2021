b3e2430b9e30b90a8668cd0baf8bb306d987bed8
Vilém Zouhar <vilem.zouhar@gmail.com>
2020-12-22 23:14:30 +0100

    filter lines, not whole documents

--- src/processing/exp1_top.py
+++ src/processing/exp1_top.py
@@ -7,33 +7,41 @@ import matplotlib.pyplot as plt
 from sklearn.linear_model import LinearRegression
 
 parser = argparse.ArgumentParser()
-parser.add_argument('-s', '--per-sent', action='store_true', default=False)
-parser.add_argument('-m', '--mt-only', action='store_true', default=False)
-parser.add_argument('-a', '--aggregate-documents', action='store_true', default=False)
-args = parser.parse_known_args()[0]
+parser.add_argument('-s', '--per-sent', action='store_true')
+parser.add_argument('-m', '--mt-only', action='store_true')
+parser.add_argument('-a', '--aggregate-documents', action='store_true')
+args = parser.parse_args()
 
 data = load_mx()
 SKIP_SRC_REF = args.mt_only
 AGGREGATE_DOCUMENTS = args.aggregate_documents
 PER_SENT = args.per_sent
-MAX_WORD_TIME = 40 
+MAX_WORD_TIME = 20
 MAX_SENT_TIME = MAX_WORD_TIME*20
 
-mt_times = {k:[] for k in sorted(MT_BLEU.keys(), key=lambda x: MT_BLEU[x][0])}
+# compute per-model data
+mt_times = {k: [] for k in sorted(MT_BLEU.keys(), key=lambda x: MT_BLEU[x][0])}
 for doc in data:
-    if not AGGREGATE_DOCUMENTS:
-        mt_times[doc.mt_name] += [x.edit_time_word for x in doc.lines if x.edit_time_word <= MAX_WORD_TIME]
+    if AGGREGATE_DOCUMENTS:
+        if PER_SENT:
+            doc_time_avg = np.average(
+                [x.edit_time for x in doc.lines if x.edit_time <= MAX_SENT_TIME]
+            )
+            mt_times[doc.mt_name].append(doc_time_avg)
+        else:
+            doc_time_avg = np.average(
+                [x.edit_time_word for x in doc.lines if x.edit_time_word <= MAX_WORD_TIME]
+            )
+            mt_times[doc.mt_name].append(doc_time_avg)
     else:
         if PER_SENT:
-            doc_time_avg = np.average([x.edit_time for x in doc.lines])
-            if doc_time_avg <= MAX_SENT_TIME:
-                mt_times[doc.mt_name].append(doc_time_avg)
+            mt_times[doc.mt_name] += [x.edit_time for x in doc.lines if x.edit_time <= MAX_SENT_TIME]
         else:
-            doc_time_avg = np.average([x.edit_time_word for x in doc.lines])
-            if doc_time_avg <= MAX_WORD_TIME:
-                mt_times[doc.mt_name].append(doc_time_avg)
+            mt_times[doc.mt_name] += [x.edit_time_word for x in doc.lines if x.edit_time_word <= MAX_WORD_TIME]
+
 
 def top_n(n, points=False):
+    # actual value plotting
     bleu_time = []
     for mt_name in list(mt_times.keys())[-n:]:
         bleus = MT_BLEU[mt_name]
@@ -41,13 +49,12 @@ def top_n(n, points=False):
             continue
         bleu_time += [(bleus[0], v) for v in mt_times[mt_name]]
 
-
     xval = [x[0] for x in bleu_time]
     yval = [x[1] for x in bleu_time]
 
     # linear fit
-    coef = np.polyfit(xval,yval,1)
-    poly1d_fn = np.poly1d(coef) 
+    coef = np.polyfit(xval, yval, 1)
+    poly1d_fn = np.poly1d(coef)
 
     if points:
         plt.plot(xval, yval, '.', alpha=0.5)
@@ -58,17 +65,20 @@ def top_n(n, points=False):
         ('(without src, ref)' if SKIP_SRC_REF else '(with src, ref)')
     )
 
-plt.figure(figsize=(5,4))
+
+# misc. plot parameters
+plt.figure(figsize=(5, 4))
 top_n(15, points=True)
 top_n(13)
 top_n(10)
 top_n(8)
 plt.legend()
 plt.title(
-        ('time per line ' if PER_SENT else 'time per word ') +
-        ('(without src, ref)' if SKIP_SRC_REF else '(with src, ref)')
+    ('time per line ' if PER_SENT else 'time per word ') +
+    ('(without src, ref)' if SKIP_SRC_REF else '(with src, ref)')
 )
 plt.xlabel('BLEU')
 perwhat = 'document' if AGGREGATE_DOCUMENTS else 'sentence'
-plt.ylabel(f'average (per {perwhat}) line edit time' if PER_SENT else f'average (per {perwhat}) word edit time')
+plt.ylabel(
+    f'average (per {perwhat}) line edit time' if PER_SENT else f'average (per {perwhat}) word edit time')
 plt.show()
--- src/processing/exp2_models.py
+++ src/processing/exp2_models.py
@@ -7,29 +7,27 @@ import matplotlib.pyplot as plt
 from sklearn.linear_model import LinearRegression
 
 parser = argparse.ArgumentParser()
-parser.add_argument('-s', '--per-sent', action='store_true', default=True)
-args = parser.parse_known_args()[0]
+parser.add_argument('-s', '--per-sent', action='store_true')
+args = parser.parse_args()
 
 data = load_mx()
 PER_SENT = args.per_sent
-MAX_WORD_TIME = 40
+MAX_WORD_TIME = 20
 MAX_SENT_TIME = MAX_WORD_TIME*20
 
 MT_ORDER = sorted(MT_BLEU.keys(), key=lambda x: MT_BLEU[x][0])
 
+# compute per-model data
 mt_times = {k: [] for k in MT_ORDER}
 for doc in data:
     if PER_SENT:
-        doc_time_avg = np.average([x.edit_time for x in doc.lines])
-        if doc_time_avg <= MAX_SENT_TIME:
-            mt_times[doc.mt_name].append(doc_time_avg)
+        mt_times[doc.mt_name] += [x.edit_time for x in doc.lines if x.edit_time <= MAX_SENT_TIME]
     else:
-        doc_time_avg = np.average([x.edit_time_word for x in doc.lines])
-        if doc_time_avg <= MAX_WORD_TIME:
-            mt_times[doc.mt_name].append(doc_time_avg)
+        mt_times[doc.mt_name] += [x.edit_time_word for x in doc.lines if x.edit_time_word <= MAX_WORD_TIME]
 
 
 def top_all():
+    # actual value plotting
     bleu_time = []
     for i, mt_name in enumerate(mt_times.keys()):
         bleus = MT_BLEU[mt_name]
@@ -41,7 +39,7 @@ def top_all():
     # linear fit
     coef = np.polyfit(xval, yval, 1)
     poly1d_fn = np.poly1d(coef)
-    plt.plot(xval, yval, 'o', alpha=0.4)
+    plt.plot(xval, yval, 'o', alpha=0.1)
     plt.plot(xval, poly1d_fn(xval), label=f'Coef: {coef[0]:>6.3f}')
     plt.xticks(range(len(MT_ORDER)), MT_ORDER, rotation=90)
     explanation = (
@@ -51,13 +49,21 @@ def top_all():
     )
 
 
+# misc. plot parameters
 plt.figure(figsize=(5, 4))
 top_all()
 plt.legend()
-plt.title(
-    ('time per line ' if PER_SENT else 'time per word ') +
-    '(with src, ref)'
-)
-plt.ylabel('average (per document) line edit time' if PER_SENT else 'average (per document) word edit time')
+TITLE = ('time per line ' if PER_SENT else 'time per word ') + '(with src, ref)'
+plt.title(TITLE)
+plt.ylabel('line edit time' if PER_SENT else 'word edit time')
 plt.tight_layout()
 plt.show()
+
+
+# print model averages
+print(TITLE)
+print('\n'.join([
+    f'{name:>10} {np.average(v):6.2f}'
+    for name, v
+    in sorted(mt_times.items(), key=lambda x: np.average(x[1]))
+]))
