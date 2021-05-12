429ad21a2b35daaa80ffd041723b48995b6ef54d
Vilém Zouhar <vilem.zouhar@gmail.com>
2021-05-12 13:20:16 +0200

    reorganize axp4

--- src/processing/axp4_square.py
+++ src/processing/axp4_square.py
@@ -3,7 +3,7 @@
 from load import *
 import numpy as np
 from collections import defaultdict
-from utils import MT_BLEU_EXT, DOMAIN_MAP, pretty_mt_name
+from utils import MT_BLEU_EXT, DOMAIN_MAP, pretty_mt_name_2
 
 parser = argparse.ArgumentParser()
 parser.add_argument('-m', '--mt-only', action='store_true')
@@ -20,18 +20,20 @@ for doc in data:
     mt_times[doc.mt_name] += [ x.lqa_distribution() for x in doc.lines ]
     domain_times[DOMAIN_MAP[doc.doc_name]] += [ x.lqa_distribution() for x in doc.lines ]
 
-# for mt_name in list(mt_times.keys()):
-#     lqa1 = np.average([x[0] for x in mt_times[mt_name]])
-#     lqa2 = np.average([x[1] for x in mt_times[mt_name]])
-#     lqa8 = np.average([x[2] for x in mt_times[mt_name]])
-#     lqaAvg = np.average([x for x in mt_times[mt_name]])
-#     print(pretty_mt_name(mt_name),
-#         "& \\blocksimple{", lqa1, "}",
-#         "& \\blocksimple{", lqa2, "}",
-#         "& \\blocksimple{", lqa8, "}",
-#         "& \\blocksimple{", lqaAvg, "}",
-#         "\\\\"
-#     )
+for mt_name in MT_BLEU_EXT.keys():
+    lqa1 = np.average([x[0] for x in mt_times[mt_name]])
+    lqa2 = np.average([x[1] for x in mt_times[mt_name]])
+    lqa8 = np.average([x[2] for x in mt_times[mt_name]])
+    lqaAvg = np.average([x for x in mt_times[mt_name]])
+    print(pretty_mt_name_2(mt_name),
+        "& \\blocksimple{", lqa1, "}",
+        "& \\blocksimple{", lqa2, "}",
+        "& \\blocksimple{", lqa8, "}",
+        "& \\blocksimple{", lqaAvg, "}",
+        "\\\\"
+    )
+
+print("\n"*5)
 
 for doc_name in list(domain_times.keys()):
     lqa1 = np.average([x[0] for x in domain_times[doc_name]])
@@ -45,24 +47,3 @@ for doc_name in list(domain_times.keys()):
         "& \\blocksimple{", lqaAvg, "}",
         "\\\\"
     )
-
-# print('\n'.join([
-#     f'{name:>10} {np.average(v):6.2f}'
-#     for name, v
-#     in sorted(mt_times.items(), key=lambda x: np.average(x[1]))
-# ]))
-
-
-# print("\nTotal avg:", np.average([np.average(x) for k,x in mt_times.items() if k not in {"google", "m10", "m11"}]))
-
-
-# print()
-# # print doc averages
-# print('\n'.join([
-#     f'{name:>10} {np.average(v):6.2f}'
-#     for name, v
-#     in sorted(doc_times.items(), key=lambda x: np.average(x[1]))
-# ]))
-
-# print("audit", np.average(doc_times["audit_i"]+doc_times["audit_r"]))
-# print("wmt", np.average(doc_times["hole"]+doc_times["china"]+doc_times["turner"]+doc_times["whistle"]))
